a43e38e7bdc183709399bc49179b7137c6debe23
Gokul Singaraju <goksing@gmail.com>
2017-09-21 12:51:55 -0400

    Adding C and Java VES Vendor libs
    Issue-Id: VNFRQTS-94
    Change-Id: Iee0e154a94f3ef4ea43efcd474acacb0fa54c43f
    Signed-Off-by: Gokul Singaraju <goksing@gmail.com>

---
+++ veslibrary/ves_javalibrary/evel_javalib2/java/com/javacodegeeks/AppTest.java
@@ -0,0 +1,16 @@
+package com.javacodegeeks;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	
+	@Test
+	public void testApp() {
+		App appObject = new App();
+		Assert.assertEquals(appObject.reverseString("Test!"), "!tseT");
+	}
+}
